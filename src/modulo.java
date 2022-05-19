223057f362f1f0e2233f25db49d88b138f6e3724
Leandro Cabral <enigmha@gmail.com>
2022-05-18 22:10:13 -0300

    criando o git e fazendo exercicio metodos

---
+++ src/modulo.java
@@ -0,0 +1,34 @@
+
+public class modulo {
+	public static void soma(double num1, double num2) {
+
+		double total = (num1 + num2);
+		
+		System.out.println("As Soma " +num1 + " + " +num2 + " é " +total);
+
+		
+	}
+
+	public static void substracao(double num1, double num2) {
+
+		double total = (num1 - num2);
+
+		System.out.println("A Substracao " +num1 + " - " +num2 + " é " +total);
+
+	}
+
+	public static void multiplcacao(double num1, double num2) {
+
+		double total = (num1 * num2);
+
+		System.out.println("A Multiplicação " +num1 + " x " +num2 + " é " +total);
+	}
+
+	public static void  divisao(double num1, double num2) {
+		
+		double total = (num1 / num2);
+		
+		System.out.println("A Divisao " +num1 + " / " +num2 + " é " +total);
+	}
+
+}
---
+++ src/operacoes.java
@@ -0,0 +1,12 @@
+
+public class operacoes {
+
+	public static void main(String[] args) {
+		modulo.soma(10, 30);
+		modulo.substracao(1000, 234);
+		modulo.divisao(100, 5);
+		modulo.multiplcacao(25, 5);
+
+	}
+
+}
